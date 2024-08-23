0916d76056a0266b409000f6e933383b7aa518a6
qserena <tom@serenander.se>
2024-08-23 13:02:34 +0200

    DbContext class.

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("EmployeeDBConnection"));
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+ 
+}
