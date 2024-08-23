8b9e49446e629b2c34369b02186db40daa5b498a
qserena <tom@serenander.se>
2024-08-23 13:24:10 +0200

    IEmployeeRepository.

---
+++ api/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+
+namespace api.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(int id);
+        Employee GetEmployeeById(int id);
+        List<Employee> GetAllEmployees();
+        void UpdateEmployee(Employee employee);
+    }
+}
