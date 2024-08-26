738902853e5375d75554ab617d4ce3222f581ab7
qserena <tom@serenander.se>
2024-08-26 16:19:06 +0200

    Adjustments to Program.cs.

--- api/Program.cs
+++ api/Program.cs
@@ -1,8 +1,14 @@
-﻿using api.Controllers;
-using Newtonsoft.Json.Serialization;
+﻿using api.Data;
+using api.Repositories; 
+//using Newtonsoft.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddCors(options => {
+    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -10,14 +16,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // 1) JSON Serializer
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options =>
-options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+//builder.Services.AddControllers().AddNewtonsoftJson(options =>
+//options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options =>
+//options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
 var app = builder.Build();
-
 // 2) Enable CORS
 app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -26,8 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
